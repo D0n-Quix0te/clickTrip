041a7902bd4df1154f5a3cb831b77da996ea9008
unknown <daniel.pontarelli@gmail.com>
2017-08-12 11:15:19 -0500

    Authentication test

--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -1,36 +1,20 @@
-// Initialize Firebase
-var config = {
-  apiKey: "AIzaSyCRSaq9IDjSHw50sM_9fMbgD2jR74DarUA",
-  authDomain: "cliptrip-24402.firebaseapp.com",
-  databaseURL: "https://cliptrip-24402.firebaseio.com",
-  projectId: "cliptrip-24402",
-  storageBucket: "cliptrip-24402.appspot.com",
-  messagingSenderId: "401198821400"
-};
-
-firebase.initializeApp(config);
-
-var database = firebase.database();
 
   var provider = new firebase.auth.GoogleAuthProvider();
-  var userId = "";
-  var name = "";
-  var email = "";
-  
+
 
   $("#loginbutton").on("click", function(event){
     event.preventDefault();
     loginGoogle();
-  }); // End of sign on with GitHub
+  }); 
 
-  // $('#signOut').on("click", function(){
-  //   signout();
-  // });
+  $('#signOut').on("click", function(){
+    signout();
+  });
 
-//Login for Github
+//Login for Google
 function loginGoogle(){
     firebase.auth().signInWithPopup(provider).then(function(result) {
-      // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+      // This gives you a Google Access Token. You can use it to access the Google API.
       var token = result.credential.accessToken;
       // The signed-in user info.
       user = result.user;
@@ -40,14 +24,16 @@ function loginGoogle(){
       // Handle Errors here.
       var errorCode = error.code;
       var errorMessage = error.message;
-      // The email of the user's acc  ount used.
+      // The email of the user's account used.
       var email = error.email;
       // The firebase.auth.AuthCredential type that was used.
       var credential = error.credential;
-        console.log("Error - " + errorCode + "  " + errorMessage + "  " + email + "  " + credential);
+      console.log("Error - " + errorCode + "  " + errorMessage + "  " + email + "  " + credential);
     });
-
 }
+
+
+
 
 //
 // function signout(){
