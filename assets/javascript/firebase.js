5ae3a38e1de249fe0c357a32b38162fea0b3c459
ewill20 <ewill2020@gmail.com>
2017-08-12 16:27:16 -0500

    small js changes

---
+++ assets/javascript/firebase.js
@@ -0,0 +1,37 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCRSaq9IDjSHw50sM_9fMbgD2jR74DarUA",
+  authDomain: "cliptrip-24402.firebaseapp.com",
+  databaseURL: "https://cliptrip-24402.firebaseio.com",
+  projectId: "cliptrip-24402",
+  storageBucket: "cliptrip-24402.appspot.com",
+  messagingSenderId: "401198821400"
+};
+
+firebase.initializeApp(config);
+
+
+var database = firebase.database();
+
+  var userId = "";
+  var name = "";
+  var email = "";
+
+  database.ref().on("value",function(snapshot) {
+  console.log(snapshot.val().userId);
+  console.log(snapshot.val().name);
+  console.log(snapshot.val().email);
+
+  })
+
+  var user = firebase.auth().currentUser;
+
+if (user != null) {
+  user.providerData.forEach(function (profile) {
+    console.log("Sign-in provider: " + profile.providerId);
+    console.log("  Provider-specific UID: " + profile.uid);
+    console.log("  Name: " + profile.displayName);
+    console.log("  Email: " + profile.email);
+    console.log("  Photo URL: " + profile.photoURL);
+  });
+}
--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -52,10 +52,12 @@ user = firebase.auth().result.user;
 
 if (user != null) {
   user.providerData.forEach(function (profile) {
-    console.log("Sign-in provider: "+profile.providerId);
-    console.log("  Provider-specific UID: "+profile.uid);
-    console.log("  Name: "+profile.displayName);
-    console.log("  Email: "+profile.email);
-    console.log("  Photo URL: "+profile.photoURL);
+    console.log("Sign-in provider: " + profile.providerId);
+    console.log("  Provider-specific UID: " + profile.uid);
+    console.log("  Name: " + profile.displayName);
+    console.log("  Email: " + profile.email);
+    console.log("  Photo URL: " + profile.photoURL);
   });
 }
+
+
