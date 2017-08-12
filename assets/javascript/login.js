aef6713404359f51422ad2e0036f51f7093c31f2
unknown <daniel.pontarelli@gmail.com>
2017-08-12 12:10:08 -0500

    no message

--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -1,14 +1,6 @@
 
   var provider = new firebase.auth.GoogleAuthProvider();
 
-// Register the callback to be fired every time auth state changes
-ref.onAuth(function(authData) {
-  if (authData) {
-    console.log("User " + authData.uid + " is logged in with " + authData.provider);
-  } else {
-    console.log("User is logged out");
-  }
-});
 
 
 
