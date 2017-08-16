547d217517a9a27abc3e4f7f7a8fade4b56fc11d
ewill20 <ewill2020@gmail.com>
2017-08-16 12:58:48 -0500

    fixes

--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -176,39 +176,12 @@ $("#upload-Button").on("click", function(event) {
     });
 
 }
+  
 
-var user = firebase.auth().currentUser;
-<<<<<<< HEAD
-    var name, email, photoUrl, uid, emailVerified;
+  var user = firebase.auth().currentUser;
+  var name, email, photoUrl, uid, emailVerified;
 
-    firebase.auth().onAuthStateChanged(function(user){
-      if (user) {
-        // User is signed in.
-      } else {
-        // No user is signed in.
-      }
- 
-    if (user != null) {
-    name = user.displayName;
-    email = user.email;
-    photoUrl = user.photoURL;
-    emailVerified = user.emailVerified;
-    uid = user.uid;  
-    $("#name-display").append(name);
-    $("#profilepic").html("<img src=" + photoUrl + ">");
-    $("#profilepic").addClass("smaller");
-    console.log(name);
-    console.log(email);
-    console.log(photoUrl);
-
-
-}
-    
-
-=======
-var name, email, photoUrl, uid, emailVerified;
-
-firebase.auth().onAuthStateChanged(function(user) {
+  firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     // User is signed in.
   } else {
@@ -228,7 +201,7 @@ firebase.auth().onAuthStateChanged(function(user) {
     console.log(email);
     console.log(photoUrl);
   }
->>>>>>> 1bcc58c26f2e0f0cf7a4b630c127f49b6f68d47e
+
 });
 
 
