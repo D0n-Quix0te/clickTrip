fdb5e06ceb59b7d5b8817996bb980d2b15ff5fbc
unknown <daniel.pontarelli@gmail.com>
2017-08-15 20:54:09 -0500

    test photos in new js

--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -101,10 +101,6 @@ $("#upload-Button").on("click", function(event) {
   //   console.log(childSnapshot.val().title);
   // })
  
-
-
-
-
   });
 
 
@@ -156,17 +152,34 @@ $("#upload-Button").on("click", function(event) {
 }
 
 
+    var user = firebase.auth().currentUser;
+    var name, email, photoUrl, uid, emailVerified;
 
-
-if (user != null) {
-  user.providerData.forEach(function (profile) {
-    console.log("Sign-in provider: " + profile.providerId);
-    console.log("  Provider-specific UID: " + profile.uid);
-    console.log("  Name: " + profile.displayName);
-    console.log("  Email: " + profile.email);
-    console.log("  Photo URL: " + profile.photoURL);
-  });
+    firebase.auth().onAuthStateChanged(function(user){
+      if (user) {
+        // User is signed in.
+      } else {
+        // No user is signed in.
+      }
+ 
+    if (user != null) {
+    name = user.displayName;
+    email = user.email;
+    photoUrl = user.photoURL;
+    emailVerified = user.emailVerified;
+    uid = user.uid;  
+    $("#name-display").append(name);
+    $("#profilepic").html("<img src=" + photoUrl + ">");
+    $("#profilepic").addclass("smaller");
+    console.log(name);
+    console.log(email);
+    console.log(photoUrl);
 }
+   
+});
+    
+
+
 
 function showImage() {
   var storageRef = firebase.storage().ref();
